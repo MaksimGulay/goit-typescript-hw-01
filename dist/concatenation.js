9446d0c5e4188d48bbcc6582c369e32b2b32e66a
Maksim Gulay <maksimgulaj@gmail.com>
2023-10-17 12:09:00 +0300

    delete ts_... folder

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -2,4 +2,5 @@ function concatenation(firstWord, secondWord) {
     console.log(`${firstWord} ${secondWord}`);
 }
 export { concatenation };
+const Maks = 10;
 //# sourceMappingURL=concatenation.js.map
--- src/concatenation.ts
+++ src/concatenation.ts
@@ -3,3 +3,5 @@ function concatenation(firstWord: string, secondWord: string) {
   }
   
   export { concatenation };
+
+  const Maks = 10
